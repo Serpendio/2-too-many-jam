2b9181df22b1a2b325ff445e1e05d531f9980d3d
Serpendio <58090288+Serpendio@users.noreply.github.com>
2024-02-20 23:42:14 +0000

    Split PlayerMovement and renamed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,30 +1,15 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting.InputSystem;
-using UnityEditor.Rendering;
-using UnityEditor.ShaderGraph;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : CreatureBase
 {
     public InputMaster controls;
 
     [Header("Movement Variables")]
-    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
     private Vector2 moveVector;
-    private float speed = 1f;
-    // Direction for animation, should default to down
-    [SerializeField] bool isFacingRight;
-    [SerializeField] bool isFacingLeft;
-    [SerializeField] bool isFacingUp;
-
-    private Vector2 lastVector;
 
 
     // Dashing - Header + SerializeField creates dropdown in Player Inspector
@@ -35,16 +20,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float dashTime;
     [SerializeField] float dashCooldown;
     private TrailRenderer dashTrail;
-    private float waitTime;
 
     // Awake is called even before Start() is called.
-    void Awake()
+    protected override void Awake()
     {
+        base.Awake();
+
         // Need to create instance of inputs object - MUST be done first thing
         controls = new InputMaster();
 
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        rb = GetComponent<Rigidbody2D>();
         dashTrail = GetComponent<TrailRenderer>();
     }
     public void Move(InputAction.CallbackContext context)
@@ -54,37 +38,20 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (isDashing)
-        {
-            return;
-        }
 
-        rb.velocity = new Vector2(moveVector.x * speed, moveVector.y * speed);
         
         //Horizontal Direction
         if (moveVector.x > 0)
         {
             isFacingRight = true;
-            isFacingLeft = false;
         }
         if (moveVector.x < 0)
         {
-            isFacingLeft = true;
             isFacingRight = false;
         }
         else
         {
-            isFacingLeft = false;
             isFacingRight = false;
-        }
-        //Up Direction
-        if (moveVector.y < 0)
-        {
-            isFacingUp = true;
-        }
-        else
-        {
-            isFacingUp = false;
         }
     }
 
@@ -96,35 +63,19 @@ public class PlayerMovement : MonoBehaviour
             return;
         }
 
-        // TEMPORARY sprite flipping
-        if (isFacingRight)
-        {
-            spriteRenderer.flipX = false;
-        }
-        else if (isFacingLeft)
-        {
-            spriteRenderer.flipX = true;
-        }
-        else if (isFacingUp)
-        {
-            spriteRenderer.flipY = true;
-        }
-        else
-        {
-            spriteRenderer.flipY = false;
-            spriteRenderer.flipX = false;
-        }
+        rb.velocity = new Vector2(moveVector.x * speed, moveVector.y * speed);
     }
 
     public void Dash(InputAction.CallbackContext context)
     {
         Debug.Log("Dash Function Triggered!");
-        
+
         if (context.performed && canDash)
         {
             StartCoroutine(Dash());
         }
     }
+
     IEnumerator Dash()
     {
         canDash = false;
