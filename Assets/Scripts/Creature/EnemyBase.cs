a94a3c6a409bbdbdb98f47ff3467f5fb27cca8e2
Serpendio <58090288+Serpendio@users.noreply.github.com>
2024-02-20 22:43:05 +0000

    Setup the enemies and creature base logic
    Also added a 2D pathfinding package

---
+++ Assets/Scripts/Creature/EnemyBase.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyBase : CreatureBase
+{
+    [SerializeField] ScriptableObject enemyData;
+    [SerializeField] private Transform target;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator anim;
+
+    // Start is called before the first frame update
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+        agent.stoppingDistance = 1;
+    }
+}
