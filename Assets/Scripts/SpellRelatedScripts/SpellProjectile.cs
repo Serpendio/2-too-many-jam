ed17f58c6fde486c4346eb34b8b5e10399b291fb
Jonathan Hurst <Jpch2k4@gmail.com>
2024-02-20 18:38:48 +0000

    Changed some constant values into variables.

--- Assets/Scripts/SpellRelatedScripts/SpellProjectile.cs
+++ Assets/Scripts/SpellRelatedScripts/SpellProjectile.cs
@@ -7,12 +7,14 @@ using UnityEngine.InputSystem;
 public class SpellProjectile : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
+    [Header("Spell Info")]
     [SerializeField] private int mana;
     [SerializeField] private float castCooldown;
+    [Header("Projectile Info")]
+    [SerializeField] private float projectileSpeed;
+    [SerializeField] private float destroyTime;
 
     public InputMaster controls;
-
-    Spell spell = new(1.2f, 2, Element.Fire);
 
     void Awake() {
         // Need to create instance of inputs object - MUST be done first thing
@@ -32,6 +34,7 @@ public class SpellProjectile : MonoBehaviour
     void Update() 
     {
         if (controls.Player.Shoot.triggered) {
+            // Gets the point in world space of the cursor, then rotates the new projectile so that it is facing the mouse
             Vector2 direction = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
             Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
@@ -39,8 +42,8 @@ public class SpellProjectile : MonoBehaviour
 
             spellShot.GetComponent<Transform>().rotation = Quaternion.Slerp(transform.rotation, rotation, 1);
             spellShot.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-            spellShot.GetComponent<Rigidbody2D>().AddForce(spellShot.GetComponent<Transform>().up * 500);
-            Destroy(spellShot, 5.0f);
+            spellShot.GetComponent<Rigidbody2D>().AddForce(spellShot.GetComponent<Transform>().up * projectileSpeed);
+            Destroy(spellShot, destroyTime);
         }
     }
 }
