8f2d9254a4ef8d16897c72bd6a4f5831a9ad9ee0
Jonathan Hurst <82543388+Linkazen@users.noreply.github.com>
2024-02-21 16:53:54 +0000

    Merged Player branch into Main (#11)
    * Added random spell generating to the spell inventory script
    
    * Added spell pickups
    
    * Merged spellmanager and player scripts
    
    Updated Player prefab and merged SpellManager into Player

--- Assets/Scripts/SpellRelatedScripts/SpellInventory.cs
+++ Assets/Scripts/SpellRelatedScripts/SpellInventory.cs
@@ -1,13 +1,50 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SpellInventory : MonoBehaviour
 {
-    public List<Spell> spells = new(10) {new Spell(5, 10)};
-    public Spell currentSpell;
+    List<Spell> spells = new(10) {new Spell(5, 10)};
+    int currentSpell;
 
     void Awake() {
-        currentSpell = spells[0];
+        currentSpell = 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.CompareTag("SpellPickup")) {
+            spells.Add(spellRand());
+            foreach (Spell spell in spells) {
+                Debug.Log(spell.element);
+            }
+        }
+    }
+
+    Spell spellRand() {
+        Spell newSpell = new();
+
+        float rNumber = UnityEngine.Random.Range(0f, 100f);
+        if (rNumber <= 50) {
+            newSpell.element = Element.none;
+        } else if (rNumber <= 60) {
+            newSpell.element = Element.Fire;
+        } else if (rNumber <= 70) {
+            newSpell.element = Element.Water;
+        } else if (rNumber <= 80) {
+            newSpell.element = Element.Lightning;
+        } else if (rNumber <= 90) {
+            newSpell.element = Element.Air;
+        } else if (rNumber <= 100) {
+            newSpell.element = Element.Earth;
+        } else  {
+            Debug.Log("ERROR - Random number generation bad");
+        }
+
+        // Rounds to 1 decimals
+        newSpell.castSpeed = math.round(UnityEngine.Random.Range(1.2f, 3f) * 10) / 10;
+
+        newSpell.manaUsage = UnityEngine.Random.Range(2, 10);
+
+        return newSpell;
     }
 }
