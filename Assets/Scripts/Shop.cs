90e773c5605c8397ad44e0a5998fef724c3f9c0b
Kahoneki <shinyzorua6@gmail.com>
2024-02-21 02:14:04 +0000

    Added unique randomised spell drops
    Using placeholder spells for now - shop doesn't communicate with any spell class

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shop : MonoBehaviour
+public class Shop : MonoBehaviour 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public GameObject[] spellIconPrefabs;
+    private Vector3[] spellLocations;
+
+    void Start() {
+        spellLocations = new[] { new Vector3(-6f, 1f, 0f), new Vector3(-1f, 1f, 0f), new Vector3(4, 1f, 0f) };
+
+        //Generate 3 random and unique spells
+        List<int> chosenSpellIndices = new List<int>();
+        for (int i = 0; i < 3; i++) {
+            //Get random spells until unique one found
+            int randIndex;
+            do
+            {
+                randIndex = Random.Range(0, spellIconPrefabs.Length);
+            } while (chosenSpellIndices.Contains(randIndex));
+
+            //Instantiate spell
+            GameObject spell = Instantiate(spellIconPrefabs[randIndex]);
+            chosenSpellIndices.Add(randIndex);
+            spell.transform.position = spellLocations[i];
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
