d0e5b58d9f65274fdd815960f7a69872fb7934f5
Kahoneki <136862746+Kahoneki@users.noreply.github.com>
2024-02-21 05:31:19 +0000

    Shop (#10)
    * Added shop.cs
    
    please work
    
    * Added unique randomised spell drops
    
    Using placeholder spells for now - shop doesn't communicate with any spell class
    
    * shop added tae world gen yippeeee
    
    fixed bug with room being made before tween uh uhh uh mhm yes
    
    * Camera resizes to fit room
    
    it sure does

---
+++ Assets/Scripts/ResizeCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using WorldGen;
+
+public class ResizeCamera : MonoBehaviour
+{
+    private void Awake()
+    {
+        Room.OnEnteredRoom.AddListener((room) => {
+            var tilemap = room.GetComponent<Tilemap>();
+            tilemap.CompressBounds();
+
+            Camera.main.transform.position = tilemap.localBounds.center + Vector3.forward * -10;
+
+            Vector3 bounds = tilemap.localBounds.size;
+
+            var screenAspect = Screen.width / Screen.height;
+            var levelAspect = bounds.x / bounds.y;
+
+            if(screenAspect > levelAspect)
+            {
+                Camera.main.orthographicSize = bounds.y / 2 * levelAspect / screenAspect;
+            } else
+            {
+                Camera.main.orthographicSize = bounds.y / 2;
+            }
+        });
+    }
+}
