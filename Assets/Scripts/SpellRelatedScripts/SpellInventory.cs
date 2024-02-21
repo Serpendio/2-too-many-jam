a72322506aeed8c962f0a7c253c545b40db82215
Jonathan Hurst <Jpch2k4@gmail.com>
2024-02-21 14:49:06 +0000

    Added random spell generating to the spell inventory script

--- Assets/Scripts/SpellRelatedScripts/SpellInventory.cs
+++ Assets/Scripts/SpellRelatedScripts/SpellInventory.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SpellInventory : MonoBehaviour
@@ -9,5 +11,33 @@ public class SpellInventory : MonoBehaviour
 
     void Awake() {
         currentSpell = spells[0];
+    }
+
+    Spell spellRand() {
+        Spell newSpell = new();
+
+        float rNumber = UnityEngine.Random.Range(0f, 100f);
+        if (rNumber <= 50) {
+            newSpell.element = Element.none;
+        } else if (rNumber <= 60) {
+            newSpell.element = Element.Fire;
+        } else if (rNumber <= 70) {
+            newSpell.element = Element.Water;
+        } else if (rNumber <= 80) {
+            newSpell.element = Element.Lightning;
+        } else if (rNumber <= 90) {
+            newSpell.element = Element.Air;
+        } else if (rNumber <= 100) {
+            newSpell.element = Element.Earth;
+        } else  {
+            Debug.Log("ERROR - Random number generation bad");
+        }
+
+        // Rounds to 1 decimals
+        newSpell.castSpeed = math.round(UnityEngine.Random.Range(1.2f, 3f) * 10) / 10;
+
+        newSpell.manaUsage = UnityEngine.Random.Range(2, 10);
+
+        return newSpell;
     }
 }
