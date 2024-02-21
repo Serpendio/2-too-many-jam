be2e28c6dfe9856221f406a58d37deaf1845e9bf
Gontarekt <Toddynator@hotmail.co.uk>
2024-02-21 19:07:53 +0000

    AudioManager Prefab
    Adding AudioManager to Prefabs

--- Assets/Scripts/Sound/Sound.cs
+++ Assets/Scripts/Sound/Sound.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [System.Serializable]
 public class Sound
 {
-    // Script stores sound names and audio clips
+    // Class stores sound names and audio clips to be used in AudioManager
     public string name;
     public AudioClip clip;
 }
