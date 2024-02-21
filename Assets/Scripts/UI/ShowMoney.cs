3b0993edb47f14a32ae2d7b0df39ad7007cb7b35
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2024-02-21 03:51:25 +0000

    Initial work on resources UI

---
+++ Assets/Scripts/UI/ShowMoney.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace UI
+{
+    public class ShowMoney : MonoBehaviour
+    {
+        private void Update()
+        {
+         // todo will use currency manager from shop branch
+        }
+    }
+}
