277a34183f3bb4ed12c621ca3e797c1992991649
Kahoneki <shinyzorua6@gmail.com>
2024-02-22 03:05:37 +0000

    Working camera
    literally everything works there is definitely no errors PLEASE oh my god omfg please bro

--- Assets/Scripts/ResizeCamera.cs
+++ Assets/Scripts/ResizeCamera.cs
@@ -14,14 +14,18 @@ public class ResizeCamera : MonoBehaviour
     float screenAspect;
     Vector2 cameraExtents;
 
+    bool firstRoom = true; //bool-lock to get startingOrthoSize
+    float startingOrthoSize; //default ortho
+
     private void Awake()
     {
-
-        transform.position = new Vector3(0,0,-10);
-        cameraExtents = new Vector2(GetComponent<Camera>().orthographicSize * screenAspect, GetComponent<Camera>().orthographicSize);
-        float orthoSize = GetComponent<Camera>().orthographicSize;
+        transform.position = new Vector3(0, 0, -10);
 
         Room.OnEnteredRoom.AddListener((room) => {
+
+            
+            cameraExtents = new Vector2(GetComponent<Camera>().orthographicSize * GetComponent<Camera>().aspect, GetComponent<Camera>().orthographicSize);
+            float orthoSize = GetComponent<Camera>().orthographicSize;
 
             Tilemap tilemap = room.GetComponent<Tilemap>();
             tilemap.CompressBounds();
@@ -33,23 +37,32 @@ public class ResizeCamera : MonoBehaviour
             screenAspect = Screen.width / Screen.height;
             float levelAspect = bounds.size.x / bounds.size.y;
 
+
             if (screenAspect > levelAspect) {
                 orthoSize = bounds.size.y / 2 * levelAspect / screenAspect;
             }
             else {
                 orthoSize = bounds.size.y / 2;
             }
+            if (firstRoom) {
+                startingOrthoSize = orthoSize;
+                firstRoom = false;
+            }
 
-            //extreme aspect ratio case - camera should follow player
-            followPlayerY = levelAspect < 0.5;
-            followPlayerX = levelAspect > 2;
+            //in case of extreme aspect ratio or very large room - camera should follow player
+            followPlayerY = levelAspect < 0.5 || bounds.size.y > 20;
+            followPlayerX = levelAspect > 2 || bounds.size.x > 20;
 
             if (!followPlayerX && !followPlayerY) {
-                if(screenAspect > levelAspect) {
+                if (screenAspect > levelAspect) {
                     GetComponent<Camera>().orthographicSize = bounds.size.y / 2 * levelAspect / screenAspect;
-                } else {
+                }
+                else {
                     GetComponent<Camera>().orthographicSize = bounds.size.y / 2;
                 }
+            }
+            else {
+                GetComponent<Camera>().orthographicSize = startingOrthoSize;
             }
 
         });
@@ -57,19 +70,20 @@ public class ResizeCamera : MonoBehaviour
 
     private void Update()
     {
-        Vector2 min = bounds.center - bounds.size / 2;
-        Vector2 max = bounds.center + bounds.size / 2;
+        Vector2 min = bounds.center - bounds.extents;
+        Vector2 max = bounds.center + bounds.extents;
 
         if (followPlayerX) {
+            Debug.Log("sdfsdkjfj");
             transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
             float clampedX = Mathf.Clamp(transform.position.x, min.x + cameraExtents.x, max.x - cameraExtents.x);
             transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
         }
-
-        else if (followPlayerY) {
+        if (followPlayerY) {
             transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
-            float clampedY = Mathf.Clamp(transform.position.y, min.y + cameraExtents.y, max.y - cameraExtents.y);
+            float clampedY = Mathf.Clamp(player.position.y, min.y + cameraExtents.y, max.y - cameraExtents.y);
             transform.position = new Vector3(transform.position.x, clampedY, transform.position.z);
         }
+
     }
 }
