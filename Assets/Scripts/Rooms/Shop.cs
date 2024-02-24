9eb8a06cea1e7aaaaeafa4caeebf6e4031ed123b
Kahoneki <shinyzorua6@gmail.com>
2024-02-24 06:37:32 +0000

    please god
    save my poor soul

--- Assets/Scripts/Rooms/Shop.cs
+++ Assets/Scripts/Rooms/Shop.cs
@@ -8,33 +8,37 @@ using UnityEngine;
 
 namespace Rooms
 {
-    public class Shop : MonoBehaviour 
+    public class Shop : MonoBehaviour
     {
 
         public GameObject[] itemIconPrefabs;
         private Vector3[] itemLocations;
 
-        void Start() {
+        void Start()
+        {
             itemLocations = new[] { new Vector3(4.5f, 8.5f, 0f), new Vector3(9.5f, 8.5f, 0f), new Vector3(14.5f, 8.5f, 0f) };
 
             //Generate 3 random and unique item-types
             List<int> chosenitemIndices = new List<int>();
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 3; i++)
+            {
                 //Get random items until unique one found
                 int randType; //0: spell, 1: modifier, 2: shard(s), 3: health refill, 4: total health increase
-                do {
+                do
+                {
                     randType = Random.Range(0, 5);
                 } while (chosenitemIndices.Contains(randType));
                 chosenitemIndices.Add(randType);
 
 
                 GameObject itemIcon = new GameObject();
-                switch (randType) {                
+                switch (randType)
+                {
                     //Spell
                     case 0:
                         Spell spell = GenerateRandomSpell();
                         break;
-                    
+
                     //Modifier
                     case 1:
                         SpellModifier modifier = SpellModifier.AllModifiers[Random.Range(0, SpellModifier.AllModifiers.Count)];
@@ -43,7 +47,7 @@ namespace Rooms
                     //Shard(s)
                     case 2:
                         break;
-                    
+
                     //Health refill
                     case 3:
                         break;
@@ -58,12 +62,14 @@ namespace Rooms
                 itemIcon.transform.position = itemLocations[i];
             }
         }
-        private Spell GenerateRandomSpell() {
+        private Spell GenerateRandomSpell()
+        {
 
-            Element randElement = (Element)Random.Range(0,6);
+            Element randElement = (Element)Random.Range(0, 6);
 
-            Spell spell = new Spell(new SpellStats {
-                DamageOnHit = Random.Range(5,25),
+            Spell spell = new Spell(new SpellStats
+            {
+                DamageOnHit = Random.Range(5, 25),
                 CastCooldown = Random.Range(0.2f, 2f),
                 ManaUsage = Random.Range(5, 25),
                 ProjectileSpeed = Random.Range(10, 40),
@@ -71,11 +77,13 @@ namespace Rooms
             }, randElement, Team.Friendly);
 
             int randNumModifiers = Random.Range(0, 3);
-            for (int i=0; i<randNumModifiers; i++) {
+            for (int i = 0; i < randNumModifiers; i++)
+            {
                 //Generate random modifiers until unique one is generated
                 SpellModifier randModifier;
-                do {
-                    randModifier = SpellModifier.AllModifiers[Random.Range(0,SpellModifier.AllModifiers.Count)];
+                do
+                {
+                    randModifier = SpellModifier.AllModifiers[Random.Range(0, SpellModifier.AllModifiers.Count)];
                 } while (!spell.Modifiers.Contains(randModifier));
                 spell.AddModifier(randModifier);
             }
