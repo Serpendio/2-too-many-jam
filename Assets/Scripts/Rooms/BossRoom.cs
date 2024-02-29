af73047c3771d3a6bda4821ede5dc7b888e10d30
Kahoneki <shinyzorua6@gmail.com>
2024-02-29 17:29:15 +0000

    Boss drops coins and shards

--- Assets/Scripts/Rooms/BossRoom.cs
+++ Assets/Scripts/Rooms/BossRoom.cs
@@ -1,4 +1,6 @@
+using Core;
 using Creature;
+using Currency;
 using Rooms;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,6 +13,8 @@ public class BossRoom : MonoBehaviour
 
     [SerializeField] private GameObject[] bossPrefabs;
     [SerializeField] private GameObject[] bossRoomEnemyPrefabs;
+    [SerializeField] private CoinDrop _coinDropPrefab;
+    [SerializeField] private ShardDrop _shardDropPrefab;
 
     private Room room;
     List<Vector3> spawnablePositions;
@@ -24,6 +28,12 @@ public class BossRoom : MonoBehaviour
     //Stage 1: Mouse
     //Stage 2: Imp
     //Stage 3: Bird
+
+    private void Awake()
+    {
+        if (_coinDropPrefab == null) _coinDropPrefab = Resources.Load<CoinDrop>("Prefabs/CoinDrop");
+        if (_shardDropPrefab == null) _shardDropPrefab = Resources.Load<ShardDrop>("Prefabs/Shard");
+    }
 
     private void Start()
     {
@@ -45,6 +55,7 @@ public class BossRoom : MonoBehaviour
             bossRoomComplete = true;
             if (Core.Locator.StageManager.getStage() < Core.Locator.StageManager.getMaxStage()) {
                 Core.Locator.StageManager.NextStage();
+                DropMoneyAndShards();
             }
             else {
                 //Win screen or smth
@@ -73,5 +84,43 @@ public class BossRoom : MonoBehaviour
             yield return new WaitForSeconds(timeBetweenEnemySpawns);
         }
         yield break;
+    }
+
+    void DropMoneyAndShards()
+    {
+        List<CoinDrop> _coinDrops = new List<CoinDrop>();
+        List<ShardDrop> _shardDrops = new List<ShardDrop>();
+        var totalValue = Locator.GameplaySettingsManager.ChestDropValue.GetValue() * 4 * Core.Locator.StageManager.getStage(); //Multiply by 4 and by stage for boss
+
+        var droppedValue = 0f;
+        while (droppedValue < totalValue)
+        {
+            var offset = transform.up * Random.Range(-0.25f, -0.75f) + transform.right * Random.Range(-0.5f, 0.5f);
+            Debug.DrawRay(transform.position, offset, Color.red, 60f);
+
+            var coinDrop = Instantiate(_coinDropPrefab, transform.position + offset, Quaternion.identity);
+            coinDrop.transform.parent = transform;
+            coinDrop.coinValue = Mathf.RoundToInt(Locator.GameplaySettingsManager.CoinDropValue.GetValue());
+            _coinDrops.Add(coinDrop);
+            droppedValue += coinDrop.coinValue;
+
+            coinDrop.OnPickup.AddListener(() =>
+            {
+                _coinDrops.Remove(coinDrop);
+            });
+
+            if (Random.value > 0.8f)
+            {
+                var shardDrop = Instantiate(_shardDropPrefab, transform.position + offset, Quaternion.identity);
+                shardDrop.transform.parent = transform;
+                shardDrop.shardValue = Random.Range(1, 4);
+                _shardDrops.Add(shardDrop);
+                droppedValue += shardDrop.shardValue;
+                shardDrop.OnPickupShard.AddListener(() =>
+                {
+                    _shardDrops.Remove(shardDrop);
+                });
+            }
+        }
     }
 }
