7e9ad14485344514b86b7471df58bf59140ebb4e
Kahoneki <shinyzorua6@gmail.com>
2024-03-17 00:13:55 +0000

    spell shard counter updates in ui

---
+++ Assets/Scripts/UI/ShardCounter.cs
@@ -0,0 +1,26 @@
+using Core;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class ShardCounter : MonoBehaviour
+    {
+        private TextMeshProUGUI _text;
+
+        private void Start()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+            UpdateCounter(Core.Locator.Inventory.Currency.ShardAmount);
+            Core.Locator.Inventory.Currency.OnSpellShardChanged.AddListener(UpdateCounter);
+        }
+
+        private void UpdateCounter(int val)
+        {
+            _text.text = val.ToString();
+        }
+    }
+}
